d9b5553374ab07ac7c08a27980937f365c51a0d3
Carlos Coelho <carlos.neto.coelho@outlook.pt>
2024-01-11 15:27:40 +0000

    created flat.js and flat.css

---
+++ src/components/flat.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./flat.css";
+
+class Flat extends React.Component {
+    render () {
+
+        const title = this.props.flat.price
+         + this.props.flat.priceCurrency + 
+         " - " + this.props.flat.name;
+
+        const style = {
+            backgroundImage: `url('${this.props.flat.imageUrl})`
+        };
+        return (
+            <div className="flat">
+                <div className="flat-picture"></div>
+                <div className="flat-title"></div>
+                {title}
+            </div>
+        ); 
+    }
+}
+
+export default Flat;
