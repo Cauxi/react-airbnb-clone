b94011d655fd69cb972e9f5a8c99c8c7b8cf32bb
Carlos Coelho <carlos.neto.coelho@outlook.pt>
2024-01-12 12:32:32 +0000

    changed app.js to acomodate for app development

--- src/components/flat.js
+++ src/components/flat.js
@@ -2,23 +2,21 @@ import React from "react";
 import "./flat.css";
 
 class Flat extends React.Component {
-    render () {
+  render() {
+    const title =
+      this.props.flat.price + this.props.flat.priceCurrency + " - " + this.props.flat.name;
 
-        const title = this.props.flat.price
-         + this.props.flat.priceCurrency + 
-         " - " + this.props.flat.name;
+    const style = {
+      backgroundImage: `url('${this.props.flat.imageUrl}')`
+    };
 
-        const style = {
-            backgroundImage: `url('${this.props.flat.imageUrl})`
-        };
-        return (
-            <div className="flat">
-                <div className="flat-picture"></div>
-                <div className="flat-title"></div>
-                {title}
-            </div>
-        ); 
-    }
+    return (
+      <div className="flat">
+        <div className="flat-picture" style={style}></div>
+        <div className="flat-title">{title}</div>
+      </div>
+    );
+  }
 }
 
 export default Flat;
